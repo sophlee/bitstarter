0ad55759e1d18bdaae227f908c7fcb6ecdcf3ed8
Sophia Lee <sophialee001@gmail.com>
2013-07-11 10:31:08 +0000

    Fixed another read from index.html bug

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  fs.readFileSync('index.html', function (err, buffer) {
+  fs.readFileSync('index.html', function (err, data) {
+      var buffer = new Buffer(data, "utf-8");
       if (err) throw err;
       response.send(buffer.toString());
   });
