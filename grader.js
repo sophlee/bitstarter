26586b6530cee134f6a12fafd3551111cb1f400c
Sophia Lee <sophia@battlefy.com>
2013-07-19 02:26:52 -0700

    Updated grader file

--- grader.js
+++ grader.js
@@ -25,6 +25,7 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
+var URLFILE_DEFAULT = "http://protected-hamlet-2673.herokuapp.com/";
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -37,6 +38,14 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(url) {
+    rest.get(url).on('complete', function(result) {
+  if (result instanceof Error) {
+      console.log("%s does not exist. Exiting.", result);
+  }
+  return result;
+    });
+}
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -66,6 +75,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to url', clone(assertUrlExists), URLFILE_DEFAULT)
         .parse(process.argv);
  
     var checkJson = checkHtmlFile(program.file, program.checks);
